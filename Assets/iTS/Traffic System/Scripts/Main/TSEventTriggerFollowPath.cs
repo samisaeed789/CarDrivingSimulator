3c1f83f9bdafc61359dd932f6e0fefdf7392bfb6
samisaeed789 <164140197+samisaeed789@users.noreply.github.com>
2024-11-18 17:08:54 +0500

    ITS LANES ADDED

---
+++ Assets/iTS/Traffic System/Scripts/Main/TSEventTriggerFollowPath.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ITS.AI
+{
+
+	public class TSEventTriggerFollowPath : TSEventTrigger
+	{
+		public bool disableCarUntilTriggeredByPlayer = false;
+		public bool disableCarPlayerSensor = false;
+		public bool endEventWithEndingPoint = false;
+		public float playerSensorTempDisableTime = 10f;
+
+		WaitForSeconds w;
+
+		public override void Awake()
+		{
+			base.Awake();
+			w = new WaitForSeconds(playerSensorTempDisableTime);
+		}
+
+		void OnTriggerEnter()
+		{
+			if (disableCarUntilTriggeredByPlayer)
+			{
+				EnableCarAI();
+				tAI.reservedForEventTrigger = false;
+				if (disableCarPlayerSensor)
+					StartCoroutine(TemporaryDisablePlayerSensor());
+			}
+
+		}
+
+		public override void InitializeMe()
+		{
+			spawnCarOnStartingPoint = true;
+		}
+
+		IEnumerator EnableCarSensorAtEndPoint()
+		{
+			while (tAI != Point(eventEndingPoint).CarWhoReserved)
+			{
+				yield return null;
+			}
+
+			//tAI.playerSensor.enabled = true;
+		}
+
+
+		IEnumerator TemporaryDisablePlayerSensor()
+		{
+			yield return null;
+			//tAI.playerSensor.enabled = false;
+			yield return w;
+			//tAI.playerSensor.enabled = true;
+		}
+
+
+		public override void SetCar(TSTrafficAI car)
+		{
+			base.SetCar(car);
+			car.ignoreTrafficLight = true;
+			if (disableCarUntilTriggeredByPlayer)
+			{
+				DisableCarAI();
+			}
+		}
+	}
+}
---
+++ Assets/iTS/Traffic System/Scripts/Main/TSRoadBlock.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using ITS.AI;
+using ITS.Utils;
+
+public class TSRoadBlock : MonoBehaviour {
+
+
+	public TSTrafficLight.TSPointReference[] blockingPoints = new TSTrafficLight.TSPointReference[0];
+
+	public TSMainManager manager;
+
+	public float roadBlockAheadDistance = 40f;
+
+	public float range = 10;
+	private int myID;
+
+	// Use this for initialization
+	void Awake () {
+		myID=TSUtils.GetUniqueID();
+		if (manager ==null)
+			manager = GameObject.FindObjectOfType<TSMainManager>();
+	}
+
+	void OnEnable()
+	{
+		if (manager !=null)
+			BlockPoints();
+	}
+
+	void OnDisable()
+	{
+		if (manager !=null){
+			UnBlockPoints();
+		}
+	}
+
+	public void BlockPoints()
+	{
+		for (int i =0; i < blockingPoints.Length;i++)
+		{
+			SetPointReservationID(blockingPoints[i],myID);
+		}
+	}
+
+
+
+	public void UnBlockPoints()
+	{
+		foreach (var point in blockingPoints)
+		{
+			UnReservePoint(point);
+		}
+	}
+
+	private void UnReservePoint(TSTrafficLight.TSPointReference point)
+	{
+		if (point.connector == -1)
+		{
+			manager.lanes[point.lane].points[point.point].TryUnReservePoint(myID);
+		}
+		else
+		{
+			manager.lanes[point.lane].connectors[point.connector].points[point.point].TryUnReservePoint(myID);
+			manager.lanes[point.lane].points[manager.lanes[point.lane].points.Length-1].TryUnReservePoint(myID);
+		}
+		
+		SetRoadBlockAhead(point, false);
+	}
+
+	private void SetPointReservationID(TSTrafficLight.TSPointReference point, int reservationID)
+	{
+		if (point.connector == -1)
+		{
+			manager.lanes[point.lane].points[point.point].TryReservePoint(reservationID, null, true);
+		}
+		else
+		{
+			manager.lanes[point.lane].connectors[point.connector].points[point.point].TryReservePoint(reservationID, null, true);
+			manager.lanes[point.lane].points[manager.lanes[point.lane].points.Length-1].TryReservePoint(reservationID, null, true);
+		}
+		SetRoadBlockAhead(point, (reservationID !=0));
+	}
+
+	private void SetRoadBlockAhead(TSTrafficLight.TSPointReference point,bool setRoadBlock)
+	{
+		float dist =0;
+		int currentPoint = point.point;
+		while (dist < roadBlockAheadDistance && currentPoint >=0)
+		{
+			manager.lanes[point.lane].points[currentPoint].roadBlockAhead = setRoadBlock;
+			dist +=manager.lanes[point.lane].points[currentPoint].distanceToNextPoint;
+			currentPoint--;
+		}
+	}
+
+
+}
